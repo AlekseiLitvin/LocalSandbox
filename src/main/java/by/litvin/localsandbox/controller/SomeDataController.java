de3ef615216162bfd2051d239c0e22bea4973d42
AliakseiLitvin <voodoochileb@gmail.com>
2022-11-01 14:46:45 -0500

    Sonar test

--- src/main/java/by/litvin/localsandbox/controller/SomeDataController.java
+++ src/main/java/by/litvin/localsandbox/controller/SomeDataController.java
@@ -3,6 +3,7 @@ package by.litvin.localsandbox.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController("/")
 public class SomeDataController {
