d9964810a336e37a3379e44adc9f0f82dc33baf6
AliakseiLitvin <voodoochileb@gmail.com>
2023-04-11 21:52:45 -0500

    Build fail investigation

--- src/test/java/by/litvin/localsandbox/IntegrationTestBase.java
+++ src/test/java/by/litvin/localsandbox/IntegrationTestBase.java
@@ -2,6 +2,7 @@ package by.litvin.localsandbox;
 
 import org.junit.jupiter.api.Tag;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -12,7 +13,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 @SpringBootTest
 // TODO enable when kafka tests will be written
-//@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 public class IntegrationTestBase {
     @Container
     public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres")
