a599cee107c63e754b6048dafb815101a8cfefae
AliakseiLitvin <voodoochileb@gmail.com>
2023-04-25 15:41:48 -0500

    Database migration script

--- utility/backup_db.sh
+++ utility/backup_db.sh
@@ -1,3 +1,5 @@
+export PATH=$PATH:$HOME/minio-binaries/
+
 if [ ! -d "$HOME/minio-binaries/mc" ]; then
   echo "Minio client was not found, downloading it"
 
@@ -6,15 +8,14 @@ if [ ! -d "$HOME/minio-binaries/mc" ]; then
     -o $HOME/minio-binaries/mc
 
   chmod +x $HOME/minio-binaries/mc
-  export PATH=$PATH:$HOME/minio-binaries/
 
   mc alias set local_minio http://ol.local:9010 root rootroot
 fi
 
 backup_filename="$(date +%s)".sql
 
-docker exec -it pg_db pg_dump vardb > "$backup_filename"
+docker exec -it pg_db pg_dump vardb > ~/jp/db_backup/"$backup_filename"
 
-mc cp "$backup_filename" local_minio/vardb-backup/"$backup_filename"
+mc cp ~/jp/db_backup/"$backup_filename" local_minio/vardb-backup/"$backup_filename"
 
 echo "Database backup was successful"
